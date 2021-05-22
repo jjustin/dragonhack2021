826bacabdb9f0029285eae0a3813402305a0b6b9
Janez Justin <janez.justin0@gmail.com>
2021-05-22 14:24:43 +0200

    fe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "antd/dist/antd.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Me from "./views/Me";
+import Stats from "./views/Stats";
+import Add from "./views/Add";
+import { Layout, Menu } from "antd";
+import {
+  QuestionOutlined,
+  BarChartOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
 
 function App() {
+  const { Content, Footer, Header } = Layout;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout className="site-layout">
+        <Header className="header">
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={
+              ["add", "prediction", "stats"].indexOf(
+                window.location.pathname.substr(1)
+              ) +
+              1 +
+              ""
+            }
+          >
+            <Menu.Item key="1" icon={<PlusOutlined />}>
+              <Link to="add">Contribute</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<QuestionOutlined />}>
+              <Link to="prediction">Prediction</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<BarChartOutlined />}>
+              <Link to="stats">Statistics</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Layout className="site-layout">
+          <Content style={{}}>
+            <div
+              className="site-layout-background"
+              style={{ textAlign: "center" }}
+            >
+              <Switch>
+                <Route path="/prediction">
+                  <Me />
+                </Route>
+                <Route path="/stats">
+                  <Stats />
+                </Route>
+                <Route path="/add">
+                  <Add />
+                </Route>
+              </Switch>
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    </Router>
   );
 }
 
---
+++ src/views/Add.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export default function Add() {
+  return (
+    <iframe
+      id="ss_widget_frame"
+      allowfullscreen=""
+      src="http://trivialgroupstudios.surveysparrow.com/widget/Vaccination-Side-Effects-Survey/tt-7979c9?"
+      style={{
+        transition: "none 0s ease 0s",
+        background: "none transparent",
+        opacity: 1,
+        border: "none",
+        zIndex: 2147483647,
+        width: "100%",
+        height: "100%",
+      }}
+    ></iframe>
+  );
+}
