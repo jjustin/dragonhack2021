1c806c82c28c639bf16e65bf32794e144c1b911f
Martin Jurkovic <mj5835@student.uni-lj.si>
2021-05-22 16:00:00 +0200

    Basic statistics view

--- src/views/Stats.js
+++ src/views/Stats.js
@@ -1,3 +1,18 @@
+import "./stats.css"
+import LineGraph from '../components/LineGraph'
+import RadarCustom from '../components/RadarCustom'
+
+
 export default function Stats() {
-  return <div>Stats</div>;
+  return (
+    <div className="statsView">
+      <div className="lineGraph">
+        <LineGraph />
+      </div>
+      <div className="radarGraph">
+        <RadarCustom />
+      </div>
+
+    </div>
+  );
 }
